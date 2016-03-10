9501fecd9a71f936785c21c4fabca9f4d409f070
vnieto <vincent.nieto@wanadoo.fr>
2016-03-10 13:01:55 +0100

    Sexier display!

--- BioNetwork.py
+++ BioNetwork.py
@@ -25,6 +25,12 @@ class Network(object):
 				self.g[j,i] = self.g[i,j] # Symmetrical matrix
 
 
+	def get_degrees(self):
+		degrees = []
+		for i in xrange(0,self.n,1):
+			degrees.append( sum(self.g[i]) )
+		return degrees
+
 
 	# Display network
 	def __str__(self):
@@ -32,31 +38,31 @@ class Network(object):
 		gx = networkx.to_networkx_graph(data=self.g)
 		# Draw the graph with Matplotlib
 		fig1 = pyplot.figure()
-		networkx.draw(gx, with_labels=False, node_size=50, linewidths=0, width=0.5, alpha=1)#, cmap=pyplot.cm.rainbow, vmin=0,vmax=1.0)
+		networkx.draw(gx, with_labels=False, node_size=50, linewidths=0, width=0.5, alpha=1, cmap=pyplot.cm.rainbow, node_color=self.get_degrees())
 		pyplot.savefig("NetworkX_plot1.png")
 		# Draw the graph with Matplotlib
 		fig2 = pyplot.figure()
-		networkx.draw_networkx(gx, with_labels=False, node_size=50, linewidths=0, width=0.5, alpha=1)
+		networkx.draw_networkx(gx, with_labels=False, node_size=50, linewidths=0, width=0.5, alpha=1, cmap=pyplot.cm.rainbow, node_color=self.get_degrees())
 		pyplot.savefig("NetworkX_plot2-networkx.png")
 		# Draw the graph with a circular layout.
 		fig3 = pyplot.figure()
-		networkx.draw_circular(gx, with_labels=False, node_size=50, linewidths=0, width=0.5, alpha=1)
+		networkx.draw_circular(gx, with_labels=False, node_size=50, linewidths=0, width=0.5, alpha=1, cmap=pyplot.cm.rainbow, node_color=self.get_degrees())
 		pyplot.savefig("NetworkX_plot3-circular.png")
 		# Draw the graph with a random layout.
 		fig4 = pyplot.figure()
-		networkx.draw_random(gx, with_labels=False, node_size=50, linewidths=0, width=0.5, alpha=1)
+		networkx.draw_random(gx, with_labels=False, node_size=50, linewidths=0, width=0.5, alpha=1, cmap=pyplot.cm.rainbow, node_color=self.get_degrees())
 		pyplot.savefig("NetworkX_plot4-random.png")
 		# Draw the graph with a spectral layout.
 		fig5 = pyplot.figure()
-		networkx.draw_spectral(gx, with_labels=False, node_size=50, linewidths=0, width=0.5, alpha=1)
+		networkx.draw_spectral(gx, with_labels=False, node_size=50, linewidths=0, width=0.5, alpha=1, cmap=pyplot.cm.rainbow, node_color=self.get_degrees())
 		pyplot.savefig("NetworkX_plot5-spectral.png")
 		# Draw the graph with a spring layout.
 		fig6 = pyplot.figure()
-		networkx.draw_spring(gx, with_labels=False, node_size=50, linewidths=0, width=0.5, alpha=1)
+		networkx.draw_spring(gx, with_labels=False, node_size=50, linewidths=0, width=0.5, alpha=1, cmap=pyplot.cm.rainbow, node_color=self.get_degrees())
 		pyplot.savefig("NetworkX_plot6-spring.png")
 		# Draw the graph with a shell layout.
 		fig7 = pyplot.figure()
-		networkx.draw_shell(gx, with_labels=False, node_size=50, linewidths=0, width=0.5, alpha=1)
+		networkx.draw_shell(gx, with_labels=False, node_size=50, linewidths=0, width=0.5, alpha=1, cmap=pyplot.cm.rainbow, node_color=self.get_degrees())
 		pyplot.savefig("NetworkX_plot7-shell.png")
 		# pyplot.draw()
 		# pyplot.show()
@@ -75,6 +81,7 @@ def main():
 	# New network:
 	n = Network(n=30)
 	print n.g
+	print n.get_degrees()
 	print n
 	
 
